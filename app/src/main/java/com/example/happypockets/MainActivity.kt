cf16f621a4988894ba5351d03c259c43b1f9b867
wz-Tan <wenzhe1423@gmail.com>
2025-01-09 12:30:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/happypockets/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.happypockets
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import com.example.happypockets.ui.theme.HappyPocketsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HappyPocketsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) {_, ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(all = Dp(80.5F)),
+
+                    )
+                }
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+    Bye()
+}
+
+@Composable
+fun Bye(){
+    Text(
+        text = "Bye Bro"
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HappyPocketsTheme {
+        Greeting("Android")
+    }
+}
